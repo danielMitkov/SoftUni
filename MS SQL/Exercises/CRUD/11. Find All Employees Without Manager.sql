273f8fa018270a8bf1a236767ecc3e2ab4fa09f6
danielMitkov <daniel.mitkov01@gmail.com>
2023-09-21 13:14:34 +0300

    Added CRUD exercises

---
+++ MS SQL/Exercises/CRUD/11. Find All Employees Without Manager.sql
@@ -0,0 +1,7 @@
+SELECT 
+    [FirstName],
+    [LastName]
+FROM 
+    [Employees]
+WHERE
+    [ManagerID] IS NULL
---
+++ MS SQL/Exercises/CRUD/13. Find 5 Best Paid Employees.sql
@@ -0,0 +1,9 @@
+SELECT TOP (5) 
+    [FirstName], 
+    [LastName]
+FROM 
+    [Employees]
+WHERE 
+    [Salary] > 50000
+ORDER BY 
+    [Salary] DESC
